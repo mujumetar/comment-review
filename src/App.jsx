524e12f46a4175af133448b8522fa17f5ef3d503
WebersIO <muzammilmetar82@gmail.com>
2025-06-09 17:43:31 +0530

    comkleyeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Task from './component/task';
+import Task from './component/Task';
 import List from './component/list';
 
 function App() {
